43756c7f3d4757ff487d6b6283059e880ec1a77f
Gulshan <125683164+gulshan-pandey@users.noreply.github.com>
2024-05-11 22:50:00 +0530

    Add files via upload

---
+++ Analog clock/script.js
@@ -0,0 +1,22 @@
+const secondsHand =document.querySelector("#second-hand");
+const minutesHand = document.querySelector("#minute-hand");
+const hoursHand = document.querySelector("#hours-hand");
+
+// console.log(secondsHand)
+
+function getTime(){
+    let now = new Date();
+    let seconds= now.getSeconds();
+    let minutes = now.getMinutes();
+    let hours =now.getHours();
+    let timeInterval =6
+    // console.log(seconds,minutes,hours)
+    secondsHand.style.transform= "rotate(+"+(seconds*timeInterval) +"deg)";
+    minutesHand.style.transform= "rotate(+"+(minutes*timeInterval) +"deg)";
+    hoursHand.style.transform = "rotate("+(hours*30+minutes/2)+"deg)";
+}
+
+getTime()
+
+setInterval(getTime,100)
+
