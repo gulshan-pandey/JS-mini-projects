42d4657c0eb6111dcf2354aec3f7ee17b8afd4a0
Gulshan <125683164+gulshan-pandey@users.noreply.github.com>
2024-05-14 00:53:29 +0530

    Update script.js

--- Guessing game/script.js
+++ Guessing game/script.js
@@ -7,7 +7,7 @@ const lastResult = document.querySelector('.lastResult')
 const lowOrHi = document.querySelector('.lowOrHi')
 const remaining = document.querySelector('.lastResult')
 const startOver = document.querySelector('.resultParas')
-let p = document.querySelector('p')
+let p = document.createElement('p')
 
 let playGame= true;
 
@@ -21,24 +21,33 @@ if(playGame){
         validateGuess(guess)
     })
 }
-function validateGuess(guess){
- if(isNaN(guess) || guess < 1 || guess > 100){
-     alert("please enter a valid number")
- }else{
-    prevGuess.push(guess);
-    checkGuess(guess);
-    if(numGuess===10){
-        displayGuess(guess)
-        displayMessage(`Game over Random number was ${randomNumber}`)
-        endGame()
-    }else{
-        displayGuess(guess)
-        checkGuess(guess)
-       
+
+
+function validateGuess(guess) {
+    if (isNaN(guess)) {
+        alert('Please enter a valid number');
+    } else if (guess < 1) {
+        alert('Please enter a number more than 1');
+    } else if (guess > 100) {
+        alert('Please enter a number less than 100');
+    } else {
+        prevGuess.push(guess);
+        checkGuess(guess);
+        if (numGuess === 11) {
+            displayGuess(guess);
+            displayMessage(`Game over. Random number was ${randomNumber}`);
+            endGame();
+        } else {
+            displayGuess(guess);
+            checkGuess(guess);
+        }
     }
- }
- 
 }
+
+
+
+
+
 
 function checkGuess(guess){
     if(guess === randomNumber){
@@ -57,7 +66,7 @@ function displayGuess(guess){
    userInput.value =""
    guessSlot.innerHTML+=`${guess} ,   `
    numGuess++
-   remaining.innerHTML = `${10-numGuess}`
+   remaining.innerHTML = `${11-numGuess}`
 }
 
 function displayMessage(message){
@@ -70,8 +79,8 @@ function endGame(){
     userInput.value=" "
     userInput.disabled = true
     p.classList.add('button')
-    p.innerHTML = `<button>Start new game</button>`
-    startOver.append(p)
+    p.innerHTML = `<button class="newGameBtn">Start new game</button>`
+    startOver.appendChild(p)
     playGame=false;
     newGame()
     
@@ -80,7 +89,7 @@ function endGame(){
 
 
 function newGame(){
-    const newGameBtn = document.querySelector('button')
+    const newGameBtn = document.querySelector('.newGameBtn')
     newGameBtn.addEventListener('click',function(e){
         randomNumber = parseInt(Math.random()*100 + 1)    // resetting everything
         lowOrHi.innerHTML = ""; // Clearing the contents of lowOrHi
