389473af9a923410a5da5f83fe36216c9433c9c6
Gulshan <125683164+gulshan-pandey@users.noreply.github.com>
2024-05-14 08:41:50 +0530

    first commit

--- Typer/script.js
+++ Typer/script.js
@@ -1,8 +1,11 @@
 let InsertWord = document.querySelector('#insert')
+let property = document.querySelector('.prop')
 let collection=''
+let type=''
 document.addEventListener('keydown',function(e){
     if(e.key==='Backspace'){
         collection=collection.slice(0,-1);
+        
     }
     else if(e.key=='Alt' || e.key=='Shift' || e.key=='Control' || e.key=="CapsLock" || e.key=="Tab"){
        e.preventDefault
@@ -12,8 +15,22 @@ document.addEventListener('keydown',function(e){
     }
     else {
         collection+= e.key
+        type = e.type
     }
     InsertWord.innerHTML = collection
+    property.innerHTML=`<table>
+    <tr>
+      <th>Key</th>
+      <th>key code</th>
+      <th>code</th>
+    </tr>
+    <tr>
+      <td>${e.key}</td>
+      <td>${e.keyCode}</td>
+      <td>${e.code}</td>
+    </tr>
+    
+  </table>`
 })
 
 
